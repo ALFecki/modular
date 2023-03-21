d1ebb5e0befc0e2ee8858b79d6050b62a22f7115
flassie <andrey1828@tut.by>
2023-03-21 19:02:36 +0300

    change dependencies

--- modular-native/src/module.rs
+++ modular-native/src/module.rs
@@ -1,8 +1,8 @@
 use crate::cstr_to_string;
 use crate::*;
 use bytes::Bytes;
-use modular::core::error::{CustomModuleError, ModuleError};
-use modular::core::modules::{ModuleRequest, ModuleResponse};
+use modular_rs::core::error::{CustomModuleError, ModuleError};
+use modular_rs::core::modules::{ModuleRequest, ModuleResponse};
 use modular_sys::*;
 use parking_lot::RwLock;
 use std::ffi::CString;
